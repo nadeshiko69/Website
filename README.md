99fb361227c91fc930c0ac74e87318e4c0827de9
sakuWin <ke.sakurauchi@gmail.com>
2024-03-18 18:05:39 +0900

    Initial commit ( https://github.com/noor1yasser9/portfolio_website/tree/master/lib )

---
+++ README.md
@@ -0,0 +1,9 @@
+# Responsive Portfolio Website
+FlutterWeb UI, We design a simple web app by using flutter, it has only a menu and some text on left side also a background image.
+
+
+### Demo Url: https://chat-app-flutter-cf389.web.app/#/
+### Video Youtub: https://www.youtube.com/watch?v=0jETqABOtu8&ab_channel=NoorEl-Nahhal
+
+## Screenshot 
+![Group 2979](https://user-images.githubusercontent.com/41232970/124959201-a6180000-e023-11eb-9a64-f83e2219c872.png)
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_website/constant/ColorsCons.dart';
+import 'package:portfolio_website/screen/component/home/HomeBanner.dart';
+import 'package:portfolio_website/screen/main/HomeScreen.dart';
+import 'package:portfolio_website/util/MenuController.dart';
+import 'package:provider/provider.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      // we are using dark theme and we modify it as our need
+      theme: ThemeData.dark().copyWith(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: bgColor,
+        canvasColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white)
+            .copyWith(
+              bodyLarge: TextStyle(color: bodyTextColor),
+              bodyMedium: TextStyle(color: bodyTextColor),
+            ),
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuControlClass(),
+          ),
+        ],
+        child: HomeScreen(
+          children: [
+            HomeBanner(),
+            // That's it for part 1
+          ],
+        ),
+      ),
+    );
+  }
+}
