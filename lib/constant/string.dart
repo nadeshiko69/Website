c4a641d9675fa7f4b517e9bd32e913c57c1b9359
sakuWin <ke.sakurauchi@gmail.com>
2024-03-22 19:01:52 +0900

    change twitter URL

--- lib/constant/string.dart
+++ lib/constant/string.dart
@@ -6,7 +6,7 @@ const LINK_IMAGE = "https://avatars.githubusercontent.com/u/46056507?v=4";
 // const LINK_BEHANCE = 'https://www.behance.net/mrh610371211';
 // const LINK_YOUTUBE = 'https://www.youtube.com/channel/UCS4VHaTpm-gv0Y1dOmzyoZA';
 const LINK_GITHUB = 'https://github.com/nadeshiko69';
-const LINK_TWITTER = 'https://twitter.com/pontoooooon';
+const LINK_TWITTER = 'https://twitter.com/sakupoyooon';
 // const LINK_CV =
 //     "https://github.com/noor1yasser9/noor1yasser9/files/6740199/2021.pdf";
 
